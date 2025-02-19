848aa946c20b0a2b375e6d9e7baa0ae03e661738
Oleksii <o.lytvynovbogdanov@gmail.com>
2025-02-19 11:55:07 +0200

    Add empty project

---
+++ MapsDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsDownloader
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
