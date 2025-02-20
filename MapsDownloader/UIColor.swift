ebc7e7eaee37575994c5dd81b8d8c0c0dc83825b
Oleksii <o.lytvynovbogdanov@gmail.com>
2025-02-20 11:52:06 +0200

    Add colors

---
+++ MapsDownloader/UIColor.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor.swift
+//  MapsDownloader
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 20.02.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static var navigationBar: UIColor { #colorLiteral(red: 1, green: 0.5333333333, blue: 0, alpha: 1) }
+    static var viewBackground: UIColor { #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9529411765, alpha: 1) }
+    static var tableCellBackground: UIColor { #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) }
+    static var separator: UIColor { #colorLiteral(red: 0.7960784314, green: 0.7803921569, blue: 0.8196078431, alpha: 1) }
+    static var iconMapDefault: UIColor { #colorLiteral(red: 0.7450980392, green: 0.7254901961, blue: 0.7725490196, alpha: 1) }
+    static var iconMapDownloaded: UIColor { #colorLiteral(red: 0.07843137255, green: 0.8, blue: 0.6196078431, alpha: 1) }
+    
+}
